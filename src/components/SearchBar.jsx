9c5fefb4bededfaa4facdaf64b3b58c665ecfa98
shaheerinit <105648279+shaheersystems@users.noreply.github.com>
2022-07-15 23:01:42 +0500

    some ui changes

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function SearchBar() {
+  const [active, setActive] = useState("two");
+  const handleOne = () => {
+    setActive("one");
+  };
+  const handleTwo = () => {
+    setActive("two");
+  };
   return (
     <div className="search-bar">
       <div className="input">
         <input type="text" placeholder="Search" />
       </div>
       <div className="display">
-        <span class="material-icons">table_rows</span>
-        <span class="material-icons active">grid_view</span>
+        <span
+          class={`material-icons ${active === "one" ? "active" : ""}`}
+          title="List view"
+          onClick={handleOne}
+        >
+          table_rows
+        </span>
+        <span
+          class={`material-icons ${active === "two" ? "active" : ""}`}
+          title="Grid View"
+          onClick={handleTwo}
+        >
+          grid_view
+        </span>
       </div>
     </div>
   );
